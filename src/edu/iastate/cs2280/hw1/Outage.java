6e2e26e2435ba14f35a23f4be64dd3e52054a8ce
Elijah Zimmerly <elijahczimmerly@gmail.com>
2026-02-18 10:57:04 -0600

    created TownCell subclasses

---
+++ src/edu/iastate/cs2280/hw1/Outage.java
@@ -0,0 +1,17 @@
+package edu.iastate.cs2280.hw1;
+
+public class Outage extends TownCell {
+
+	@Override
+	public State who() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TownCell next(Town tNew) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
